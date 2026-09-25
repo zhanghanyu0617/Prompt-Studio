f8c342e9f69ecb1f28221563747b43bc65b8ae24
zhanghanyu0617 <3766253251@qq.com>
2026-09-25 22:11:53 +0800

    chore: 将 .open-next 构建产物加入 .gitignore

--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/user/Doubao/chats/38444079574579458/Prompt-Studio/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
